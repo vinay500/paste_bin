364a14fe4b1a90fd1ae8fafc288e4d95ec7d7859
vinay-prodify <vinaym@prodify.co.in>
2026-02-01 22:27:57 +0530

    serving client from server

--- server/src/app.js
+++ server/src/app.js
@@ -10,6 +10,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "../../client/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../client/dist/index.html"));
+});
+
 const { getPaste } = require('./pasteService');
 
 // View Paste (HTML)
