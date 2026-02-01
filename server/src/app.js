6e0626f970e2edc7d4f25439f7348646b2868598
vinay-prodify <vinaym@prodify.co.in>
2026-02-01 22:34:40 +0530

    changed app.js

--- server/src/app.js
+++ server/src/app.js
@@ -14,7 +14,12 @@ const path = require("path");
 
 app.use(express.static(path.join(__dirname, "../../client/dist")));
 
-app.get("*", (req, res) => {
+// app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../../client/dist/index.html"));
+// });
+
+
+app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "../../client/dist/index.html"));
 });
 
